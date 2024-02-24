b802ff08ad59b70d41ca05cf6454c61d28bdd4c6
Cinematec <148294568+cinematec@users.noreply.github.com>
2024-02-24 18:17:28 -0300

    update js/boxMsg.js

--- js/boxMsg.js
+++ js/boxMsg.js
@@ -1,6 +1,13 @@
 import Janela from "./janela.js";
-
+const molduras = [...document.querySelectorAll("legend")];
+const spans = [...document.querySelectorAll("#span")];
 const botaoMsg = document.getElementById("botaoMsg");
 botaoMsg.addEventListener("click", () => {
+  molduras.forEach((moldura) => {
+    moldura.style.opacity = 0;
+  });
+  spans.forEach((span) => {
+    span.style.opacity = 0.5;
+  });
   Janela.exibir();
 });
