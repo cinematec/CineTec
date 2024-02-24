28fd2f8f0bc1a46da9544f8bcfafaddb0481cb7d
Cinematec <148294568+cinematec@users.noreply.github.com>
2024-02-24 18:18:02 -0300

    update js/janela.js

--- js/janela.js
+++ js/janela.js
@@ -2,7 +2,7 @@ class Janela {
   static header_content = "Titulo";
   static main_content = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid similique fuga rerum excepturi, amet, quibusdam laudantium praesentium iure aspernatur. Doloremque amet pariatur eius dicta accusantium id consequuntur quas sequi rerum.";
   static footer_content = "&copyIago";
-  static color = "#939393";
+  static color = "#1e1e1e";
   static destino = document.body;
   
  /* config(config) {
@@ -31,6 +31,7 @@ class Janela {
     "background-color: #fff;" +
     "border-radius: 6px;" +
     "width: 300px;" +
+    "opacity: 100%;" +
     "display: flex;" +
     "align-items: center;" +
     "justify-content: center;" +
@@ -38,9 +39,10 @@ class Janela {
     box_msg.setAttribute("style", style_box_msg);
     area_msg.prepend(box_msg);
     
-    const header = document.createElement("header");
+    const header = document.createElement("div");
     const style_header = 
     "background-color:"+this.color+";" +
+    "color: #fff;" +
     "border-radius: 0px;" +
     "width: 100%;" +
     "display: flex;" +
@@ -50,6 +52,35 @@ class Janela {
     header.innerHTML = this.header_content;
     header.setAttribute("style", style_header);
     box_msg.prepend(header);
+    
+    const main = document.createElement("div");
+    const style_main = 
+    "background-color: #fff;" +
+    "border-radius: 0px;" +
+    "width: 100%;" +
+    "display: flex;" +
+    "align-items: center;" +
+    "justify-content: start;" +
+    "text-align: center;" +
+    "flex-direction: column;";
+    main.innerHTML = this.main_content;
+    main.setAttribute("style", style_main);
+    box_msg.appendChild(main);
+    
+    const footer = document.createElement("div");
+    const style_footer = 
+    "background-color:"+this.color+";" +
+    "color: #fff;" +
+    "border-radius: 0px;" +
+    "width: 100%;" +
+    "display: flex;" +
+    "align-items: center;" +
+    "justify-content: start;" +
+    "text-align: center;" +
+    "flex-direction: column;";
+    footer.innerHTML = this.footer_content;
+    footer.setAttribute("style", style_footer);
+    box_msg.appendChild(footer);
   }
   static ocultar() {
     this.destino.remove(area_msg);
