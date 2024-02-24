e25400c59571db85ed956c40c9cc3edadacc78fa
Cinematec <148294568+cinematec@users.noreply.github.com>
2024-02-24 15:52:59 -0300

    update js/janela.js

--- js/janela.js
+++ js/janela.js
@@ -12,20 +12,47 @@ class Janela {
     const area_msg = document.createElement("div");
     const style_area = 
     "background-color: rgba(0,0,0,0.7);" +
-    "height: 100vh;" +
+    "height: 100%;" +
     "width: 100%;" +
+    "position: absolute;" +
+    "top: 0px;" +
+    "left: 0px;" +
     "display: flex;" +
     "align-items: center;" +
     "justify-content: center;" +
     "flex-direction: column;";
     
     area_msg.setAttribute("style",style_area);
-    this.destino.appendChild(area_msg);
+    this.destino.prepend(area_msg);
+    this.destino.style.overflowY = "hidden";
+    
+    const box_msg = document.createElement("div");
+    const style_box_msg = 
+    "background-color: #fff;" +
+    "border-radius: 6px;" +
+    "width: 300px;" +
+    "display: flex;" +
+    "align-items: center;" +
+    "justify-content: center;" +
+    "flex-direction: column;";
+    box_msg.setAttribute("style", style_box_msg);
+    area_msg.prepend(box_msg);
     
     const header = document.createElement("header");
+    const style_header = 
+    "background-color:"+this.color+";" +
+    "border-radius: 0px;" +
+    "width: 100%;" +
+    "display: flex;" +
+    "align-items: center;" +
+    "justify-content: center;" +
+    "flex-direction: column;";
+    header.innerHTML = this.header_content;
+    header.setAttribute("style", style_header);
+    box_msg.prepend(header);
   }
   static ocultar() {
-    
+    this.destino.remove(area_msg);
   }
 }
 export default Janela;
