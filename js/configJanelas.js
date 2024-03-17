a09703093049eaf28e56810b7d5e95c01a01787c
Cinematec <148294568+cinematec@users.noreply.github.com>
2024-03-16 21:12:39 -0300

    update js/configJanelas.js

--- js/configJanelas.js
+++ js/configJanelas.js
@@ -1,4 +1,4 @@
-import Janela from "https://cdn.jsdelivr.net/gh/theus-iago/prototypes/window_v6.js";
+import Janela from "https://cdn.jsdelivr.net/gh/theus-iago/prototypes/window_v35.js";
 
 const molduras = [...document.querySelectorAll("legend")];
 const spans = [...document.querySelectorAll("#span")];
